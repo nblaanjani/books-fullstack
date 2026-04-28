8146aa2427ae185fa19de449813be2322a37a8b5
nblaanjani <nblaanjani@gmail.com>
2026-04-28 19:45:15 +0700

    first commit - public API Laravel

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\PublicPostController;
 
-Route::apiResource('books', BookController::class);
+Route::get('/public-posts', [PublicPostController::class, 'getAllPosts']);
